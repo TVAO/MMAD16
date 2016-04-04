22b3a9ef9aabcc88c70a6f974aad3239b40acf22
TVANO <thorolesen14@gmail.com>
2016-04-04 21:15:23 +0200

    Added code to get product data in ThingListFragment.
    Added network code to get product info in ProductFetcher.
    
    Need to adjust it to JSON format in Outpan and use barcode and name intially to set fields when scanning a new item.
    
    Finally need to make the network robust by checking for connection and make a preference activity page to control network usage.

--- app/src/main/java/tvao/mmad/itu/tingle/Controller/Fragments/ThingListFragment.java
+++ app/src/main/java/tvao/mmad/itu/tingle/Controller/Fragments/ThingListFragment.java
@@ -3,6 +3,7 @@ package tvao.mmad.itu.tingle.Controller.Fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -25,12 +26,14 @@ import com.bignerdranch.android.multiselector.ModalMultiSelectorCallback;
 import com.bignerdranch.android.multiselector.MultiSelector;
 import com.bignerdranch.android.multiselector.SwappingHolder;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
 import tvao.mmad.itu.tingle.Controller.Activities.ThingPagerActivity;
 import tvao.mmad.itu.tingle.Model.Thing;
 import tvao.mmad.itu.tingle.Model.ThingRepository;
+import tvao.mmad.itu.tingle.ProductFetcher;
 import tvao.mmad.itu.tingle.R;
 
 
@@ -483,6 +486,54 @@ public class ThingListFragment extends Fragment {
         {
             mThings = things;
         }
+    }
+
+    /**
+     * Used to work with background threads using AsyncTask.AsyncTask class that creates a background thread and runs code in doInBackground on that thread.
+     * This inner class override AsyncTask.doInBackground to get product data from Outpan website and log it.
+     * First parameter is input parameters (e.g. barcode),
+     * Second parameter is a specific type for sending progress updates (not used),
+     * Third parameter is the result from fetching data (e.g. JSON object with product info)
+     */
+    private class FetchOutpanTask extends AsyncTask<String, Void, byte[]> {
+        @Override
+        protected byte[] doInBackground(String... params)
+        {
+            byte[] result = null;
+
+            try
+            {
+                // Example: https://www.outpan.com/view_product.php?barcode=0076808501063
+                result = new ProductFetcher().getUrlBytes("https://www.outpan.com/");
+
+                // https://api.outpan.com/v2/products/[barcode]/?apikey=[key]
+//                result = new ProductFetcher().
+//                        getUrlBytes("https://api.outpan.com/v2/products/" + params[0] + "/?apikey=[KEY]");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            }
+            catch (IOException ioe)
+            {
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+
+            new ProductFetcher().fetchProducts();
+
+            //return null;
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(byte[] products)
+        {
+            updateUI();
+        }
+
+//        @Override
+//        protected void onPostExecute(List<Thing> products)
+//        {
+//            mAdapter.setThings(products);
+//            updateUI();
+//        }
     }
 
 }
