98f2aa4a31428823836bcc2f95164bb8cc5f42e7
TVANO <thorolesen14@gmail.com>
2016-04-16 08:17:20 +0200

    Added documentation to DatePicker fragment

--- app/src/main/java/tvao/mmad/itu/tingle/Controller/Fragments/DatePickerFragment.java
+++ app/src/main/java/tvao/mmad/itu/tingle/Controller/Fragments/DatePickerFragment.java
@@ -18,16 +18,26 @@ import java.util.GregorianCalendar;
 import tvao.mmad.itu.tingle.R;
 
 /**
- * This fragments represents a dialog.
+ * This fragments represents a dialog providing the user a DatePicker widget to select a date when registering a thing.
+ * The DatePicker is used in ThingDetailFragment when detailing information about a thing.
  */
 public class DatePickerFragment extends DialogFragment {
 
     private static final String ARG_DATE = "date";
     public static final String EXTRA_DATE = "itu.mmad.dttn.tingle.date";
 
-
     private DatePicker mDatePicker;
 
+    /**
+     * This method is used to instantiate a new Fragment used to display a date picking dialogue screen.
+     * Encapsulates and abstracts the steps required to setup the object from the client.
+     *
+     * Rather than having the client call the default constructor and manually set the fragment's arguments themselves,
+     * we provide a static factory method that does this for them making fragment instantiation convenient and enforcing well-defined behavior.
+     *
+     * @param date - date related to thing shown in activity.
+     * @return - new fragment with thing details including date.
+     */
     public static DatePickerFragment newInstance(Date date)
     {
         Bundle args = new Bundle();
@@ -38,16 +48,22 @@ public class DatePickerFragment extends DialogFragment {
         return fragment;
     }
 
+    /**
+     * Call to do initial creation of fragment
+     * @param savedInstanceState - fragment rebuilt from saved state if not null
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
         Date date = (Date) getArguments().getSerializable(ARG_DATE);
 
+        // Use current time as default values for picker
         Calendar calender = Calendar.getInstance();
         calender.setTime(date);
         int year = calender.get(Calendar.YEAR);
@@ -60,6 +76,7 @@ public class DatePickerFragment extends DialogFragment {
         mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_picker);
         mDatePicker.init(year, month, day, null);
 
+        // Setup an Alert Dialogue to avoid leaving the detail window while picking a date
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
@@ -78,8 +95,11 @@ public class DatePickerFragment extends DialogFragment {
                 .create();
     }
 
-    private void sendResult(int resultCode, Date date){
-        if(getTargetFragment() == null){
+    // Send result of date picker to target fragment ThingDetailFragment
+    private void sendResult(int resultCode, Date date)
+    {
+        if(getTargetFragment() == null)
+        {
             return;
         }
         Intent intent = new Intent();
