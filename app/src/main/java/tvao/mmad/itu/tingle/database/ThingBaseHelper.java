899d3c9d1a5a0fc3e88d624b504beeaa727e634c
TVANO <thorolesen14@gmail.com>
2016-04-17 21:32:53 +0200

    cleanup

--- app/src/main/java/tvao/mmad/itu/tingle/database/ThingBaseHelper.java
+++ app/src/main/java/tvao/mmad/itu/tingle/database/ThingBaseHelper.java
@@ -59,6 +59,7 @@ public class ThingBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
         db.execSQL("DROP TABLE IF EXISTS" + ThingTable.NAME);
+        this.onCreate(db);
     }
 
 }
